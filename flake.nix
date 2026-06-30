2afeb96245e550703eccc44ed100ec47493f0003
Bart Oostveen <bart@bartoostveen.nl>
2026-07-01 01:13:11 +0200

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,126 @@
+{
+  description = "search.boostveen.nl";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    search = {
+      url = "github:nuschtos/search";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "authentik-nix/systems";
+    };
+
+    # Search scopes
+
+    authentik-nix = {
+      url = "github:nix-community/authentik-nix";
+      inputs = {
+        flake-compat.follows = "";
+        flake-parts.follows = "flake-parts";
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    comin = {
+      url = "github:nlewo/comin";
+      inputs = {
+        flake-compat.follows = "";
+        nixpkgs.follows = "nixpkgs";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+    copyparty = {
+      url = "github:9001/copyparty";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    meshcoretomqtt = {
+      url = "github:Cisien/meshcoretomqtt";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+    # nix-oci-lock = { # TODO
+    #   url = "git+ssh://forgejo@git.bartoostveen.nl/bart/nix-oci-lock.git";
+    #   inputs = {
+    #     flake-parts.follows = "flake-parts";
+    #     nixpkgs.follows = "nixpkgs";
+    #     treefmt-nix.follows = "treefmt-nix";
+    #   };
+    # };
+    simple-nixos-mailserver = {
+      url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
+      inputs = {
+        flake-compat.follows = "";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          self',
+          inputs',
+          ...
+        }:
+
+        {
+          treefmt = {
+            programs.nixfmt.enable = true;
+          };
+
+          packages.default = pkgs.callPackage ./package.nix { inherit inputs inputs'; };
+          packages.search = self'.packages.default;
+        };
+    };
+}
---
+++ package.nix
@@ -0,0 +1,119 @@
+{
+  inputs,
+  inputs',
+  lib,
+  nixosOptionsDoc,
+  jq,
+  runCommand,
+  pkgs,
+}:
+
+let
+  inherit (lib) getExe;
+
+  includePkgs = {
+    _module.args = { inherit pkgs; };
+  };
+in
+inputs'.search.packages.mkMultiSearch {
+  scopes = [
+    {
+      modules = [
+        inputs.authentik-nix.nixosModules.default
+        includePkgs
+      ];
+      name = "authentik-nix";
+      pkgs = inputs'.authentik-nix.packages;
+      urlPrefix = "https://github.com/nix-community/authentik-nix/blob/main/";
+    }
+    {
+      modules = [ inputs.comin.nixosModules.comin ];
+      name = "comin";
+      pkgs = inputs'.comin.packages;
+      urlPrefix = "https://github.com/nlewo/comin/blob/main/";
+    }
+    {
+      name = "copyparty";
+      urlPrefix = "https://github.com/9001/copyparty/blob/hovudstraum/";
+      modules = [ inputs.copyparty.nixosModules.default ];
+    }
+    {
+      modules = [ inputs.disko.nixosModules.default ];
+      name = "disko";
+      pkgs = inputs'.disko.packages;
+      specialArgs.modulesPath = inputs.nixpkgs + "/nixos/modules";
+      urlPrefix = "https://github.com/nix-community/disko/blob/master/";
+    }
+    {
+      optionsJSON =
+        inputs'.home-manager.packages.docs-html.passthru.home-manager-options.nixos.json
+        + /share/doc/nixos/options.json;
+      name = "Home Manager (NixOS)";
+      urlPrefix = "https://github.com/nix-community/home-manager/tree/master/";
+    }
+    {
+      optionsJSON = inputs'.home-manager.packages.docs-json + /share/doc/home-manager/options.json;
+      optionsPrefix = "home-manager.users.<name>";
+      name = "Home Manager (Standalone)";
+      urlPrefix = "https://github.com/nix-community/home-manager/tree/master/";
+    }
+    {
+      modules = [
+        inputs.meshcoretomqtt.nixosModules.default
+        includePkgs
+      ];
+      name = "Meshcore to MQTT";
+      urlPrefix = "https://github.com/Cisien/meshcoretomqtt/blob/main/";
+    }
+    {
+      name = "plasma-manager";
+      urlPrefix = "https://github.com/nix-community/plasma-manager/blob/main/";
+      optionsJSON =
+        let
+          eval = inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              inputs.plasma-manager.homeModules.plasma-manager
+              {
+                home.stateVersion = "26.11";
+                home.username = "someuser";
+                home.homeDirectory = "/home/someuser";
+              }
+            ];
+          };
+          doc = nixosOptionsDoc {
+            inherit (eval) options;
+            warningsAreErrors = false;
+          };
+        in
+        runCommand "options-filtered" { } ''
+          ${getExe jq} 'to_entries | map(select(.key | startswith("programs.plasma"))) | from_entries' ${doc.optionsJSON}/share/doc/nixos/options.json > $out
+        '';
+    }
+    {
+      modules = [
+        inputs.simple-nixos-mailserver.nixosModules.default
+        # based on https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/blob/290a995de5c3d3f08468fa548f0d55ab2efc7b6b/flake.nix#L61-73
+        {
+          mailserver = {
+            fqdn = "mx.example.com";
+            domains = [ "example.com" ];
+            dmarcReporting = {
+              organizationName = "Example Corp";
+              domain = "example.com";
+            };
+          };
+        }
+      ];
+      name = "simple-nixos-mailserver";
+      pkgs = inputs'.simple-nixos-mailserver.packages;
+      urlPrefix = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/blob/master/";
+    }
+    {
+      modules = [ inputs.sops-nix.nixosModules.default ];
+      name = "sops-nix";
+      pkgs = inputs'.sops-nix.packages;
+      urlPrefix = "https://github.com/Mic92/sops-nix/blob/master/";
+    }
+  ];
+}
